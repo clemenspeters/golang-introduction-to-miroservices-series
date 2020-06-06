1708b5add96f3aa64205c7b2e4cc3e7edff73736
Clemens Peters <clemenspeters@gmx.de>
2020-06-06 22:06:03 +0200

    Add getNextId method.

--- introduction-to-miroservices-series/episode4/data/products.go
+++ introduction-to-miroservices-series/episode4/data/products.go
@@ -34,6 +34,11 @@ func GetProducts() Products {
 	return productList
 }
 
+func getNextId() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
+}
+
 var productList = []*Product{
 	&Product{
 		ID:          1,
