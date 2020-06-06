7e124d8e902cafa2c81459f8419161bef2f44282
Clemens Peters <clemenspeters@gmx.de>
2020-06-06 18:29:28 +0200

    Use new handler on custom custom ServeMux.

--- introduction-to-miroservices-series/episode2/main.go
+++ introduction-to-miroservices-series/episode2/main.go
@@ -1,27 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/clemenspeters/golang-tutorials/introduction-to-miroservices-series/episode2/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World")
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(rw, "Oops", http.StatusBadRequest)
-			return
-		}
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
 
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
 
-	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Goodbye World")
-	})
-
-	http.ListenAndServe(":9090", nil)
+	http.ListenAndServe(":9090", sm)
 }
