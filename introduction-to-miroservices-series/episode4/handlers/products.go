1816b76be78093cd1118f1d848893c1ff684d2fe
Clemens Peters <clemenspeters@gmx.de>
2020-06-06 21:24:06 +0200

    :sparkles: Add POST endpoint.

--- introduction-to-miroservices-series/episode4/handlers/products.go
+++ introduction-to-miroservices-series/episode4/handlers/products.go
@@ -21,14 +21,24 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+	}
+
 	// catch all
+	// if no method is satisfied return an error
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET products")
 	lp := data.GetProducts()
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to convert json", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST products")
 }
