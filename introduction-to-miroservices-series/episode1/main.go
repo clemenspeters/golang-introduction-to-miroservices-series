944bf34c25b4b4090fa89cd97448c9364f6a02bd
Clemens Peters <clemenspeters@gmx.de>
2020-06-06 18:06:22 +0200

    Add error handling.

--- introduction-to-miroservices-series/episode1/main.go
+++ introduction-to-miroservices-series/episode1/main.go
@@ -10,7 +10,12 @@ import (
 func main() {
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		log.Println("Hello World")
-		d, _ := ioutil.ReadAll(r.Body)
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Oops", http.StatusBadRequest)
+			return
+		}
+
 		fmt.Fprintf(rw, "Hello %s", d)
 	})
 
