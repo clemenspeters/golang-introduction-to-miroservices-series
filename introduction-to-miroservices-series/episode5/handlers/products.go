f19bb3cea53f0091be0057fbc093cd316c9a58af
Clemens Peters <clemenspeters@gmx.de>
2020-06-06 23:41:00 +0200

    :recycle: Add back PUT method.
    Using mux.Vars to read the id from the URI.

--- introduction-to-miroservices-series/episode5/handlers/products.go
+++ introduction-to-miroservices-series/episode5/handlers/products.go
@@ -3,8 +3,10 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/clemenspeters/golang-tutorials/introduction-to-miroservices-series/episode5/data"
+	"github.com/gorilla/mux"
 )
 
 type Products struct {
@@ -38,12 +40,19 @@ func (p *Products) AddProduct(rw http.ResponseWriter, r *http.Request) {
 	data.AddProduct(prod)
 }
 
-func (p *Products) UpdateProducts(id int, rw http.ResponseWriter, r *http.Request) {
-	p.l.Println("Handle PUT products")
+func (p *Products) UpdateProducts(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(rw, "Unable to convert ID", http.StatusBadRequest)
+		return
+	}
+
+	p.l.Println("Handle PUT product", id)
 
 	prod := &data.Product{}
 
-	err := prod.FromJSON(r.Body)
+	err = prod.FromJSON(r.Body)
 	if err != nil {
 		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
 		return
