d1012232abff2a0a6d6ec6fe88ee20166dcae54c
Clemens Peters <clemenspeters@gmx.de>
2020-06-06 19:35:41 +0200

    :pencil2: Fix import typo.

--- introduction-to-miroservices-series/episode3/main.go
+++ introduction-to-miroservices-series/episode3/main.go
@@ -8,7 +8,7 @@ import (
 	"os/signal"
 	"time"
 
-	"github.com/clemenspeters/golang-tutorials/introduction-to-miroservices-series/episode2/handlers"
+	"github.com/clemenspeters/golang-tutorials/introduction-to-miroservices-series/episode3/handlers"
 )
 
 func main() {
