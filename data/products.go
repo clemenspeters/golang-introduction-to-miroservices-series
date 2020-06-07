da05d903a324422c0ef637aea9f839a0af3939eb
Clemens Peters <clemenspeters@gmx.de>
2020-06-07 12:57:36 +0200

    Add custom validation for custom format of SKU.

--- data/products.go
+++ data/products.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"regexp"
 	"time"
 
 	"github.com/go-playground/validator"
@@ -15,7 +16,7 @@ type Product struct {
 	Name        string  `json:"name" validate:"required"`
 	Description string  `json:"description"`
 	Price       float32 `json:"price" validate:"gt=0"`
-	SKU         string  `json:"sku"`
+	SKU         string  `json:"sku" validate:"required,sku"`
 	CreatedOn   string  `json:"-"`
 	UpdatedOn   string  `json:"-"`
 	DeleteOn    string  `json:"-"`
@@ -28,7 +29,20 @@ func (p *Product) FromJSON(r io.Reader) error {
 
 func (p *Product) Validate() error {
 	validate := validator.New()
+	validate.RegisterValidation("sku", validateSKU)
 	return validate.Struct(p)
+}
+
+func validateSKU(fl validator.FieldLevel) bool {
+	// sku is of format abc-absd-dfsdf
+	re := regexp.MustCompile(`[a-z]+-[a-z]+-[a-z]`)
+	matches := re.FindAllString(fl.Field().String(), -1)
+
+	if len(matches) != 1 {
+		return false
+	}
+
+	return true
 }
 
 type Products []*Product
