1baa52cab77a07fad9462457e65c8bd1a1ed6bb6
Clemens Peters <clemenspeters@gmx.de>
2020-06-07 13:06:05 +0200

    :lock: Add validation to the middleware.

--- handlers/products.go
+++ handlers/products.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -75,7 +76,20 @@ func (p Products) MiddlewareProductValidation(next http.Handler) http.Handler {
 
 		err := prod.FromJSON(r.Body)
 		if err != nil {
+			p.l.Println("Error:", err)
 			http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+			return
+		}
+
+		// validate the product
+		err = prod.Validate()
+		if err != nil {
+			p.l.Println("Invalid product json object. Error:", err)
+			http.Error(
+				rw,
+				fmt.Sprintf("Invalid product json object: %s", err),
+				http.StatusBadRequest,
+			)
 			return
 		}
 
